de466fba03e5737f0c3e30df8fb9cf0d76cc8eca
Timo Reitz <godsboss@gmx.de>
2020-03-29 19:29:03 +0200

    Remove leftover imports.

--- src/main/java/org/godsboss/minecraft/mods/ThermalExpansionPlusEmbersMod.java
+++ src/main/java/org/godsboss/minecraft/mods/ThermalExpansionPlusEmbersMod.java
@@ -2,9 +2,7 @@ package org.godsboss.minecraft.mods;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.godsboss.minecraft.mods.init.Recipes;
 
 @Mod(
